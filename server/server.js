0c6ac8019494d12ad63bddec51a10c6a7c262f66
mithleshfreak <best1466@gmail.com>
2018-02-08 14:39:00 +0545

    test

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 var port = process.env.PORT || 3000;
 const path = require('path');
+const http = require('http');
 const express = require('express');
 
+const socketIO = require('socket.io');
+
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(path.join(__dirname,'../public')));
 
 const publicPath = path.join(__dirname,'../public');
 
 
-app.listen(port ,()=>{
+io.on('connection',(socket)=>{
+  console.log('New User Connected');
+});
+
+server.listen(port,()=>{
   console.log(`Server running on port ${port} `);
 })
