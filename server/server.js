8c28e9b96ae03224e623bdfbe3c842fc142e6ad3
mithleshfreak <best1466@gmail.com>
2018-02-08 14:52:23 +0545

    Connect And Disconnect

--- server/server.js
+++ server/server.js
@@ -16,7 +16,12 @@ const publicPath = path.join(__dirname,'../public');
 
 io.on('connection',(socket)=>{
   console.log('New User Connected');
+  socket.on('disconnect',()=>{
+    console.log('User Disconnected');
+  });
 });
+
+
 
 server.listen(port,()=>{
   console.log(`Server running on port ${port} `);
